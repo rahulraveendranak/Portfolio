beaf43a6be1520c8a5821347fa5493a4510238c6
Rahul <rahular60@gmail.com>
2020-08-13 12:07:02 +0530

    basic portfolio/added form table/basic bootstrap

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,HttpResponse
+from home.models import Contact
+
+# Create your views here.
+def home(request):       
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def projects(request):
+    return render(request,'projects.html')
+
+def contact(request):
+    if request.method == "POST":        
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        desc=request.POST['desc']
+        ins= Contact(name=name,email=email,phone=phone,desc=desc)
+        ins.save()
+        print(name)
+    return render(request,'contact.html')
